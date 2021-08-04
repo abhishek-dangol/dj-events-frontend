7e777f626635e0adf44119bbe4df95ee70379780
abhishek-dangol <abhi.dangol@gmail.com>
2021-08-03 22:33:13 -0500

    Displayed events in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Layout from '@/components/Layout'
-import {API_URL} from '@/config/index'
+import { API_URL } from '@/config/index'
+import EventItem from '@/components/EventItem';
+import Link from 'next/link'
 
 export default function HomePage({events}) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}></EventItem>
+
+      ))}
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -14,7 +26,7 @@ export async function getStaticProps() {
   const events = await res.json()
 
   return {
-    props: { events },
+    props: { events:events.slice(0, 3) },
     revalidate: 1,
   }
 }
