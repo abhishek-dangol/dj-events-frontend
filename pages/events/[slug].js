68e763614dfabeef90c42c3180f027539d35a93d
abhishek-dangol <abhi.dangol@gmail.com>
2021-08-09 17:02:25 -0500

    added delete function

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -3,55 +3,73 @@ import { API_URL } from "@/config/index";
 import styles from '@/styles/Event.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
-import {FaPencilAlt, FaTimes} from 'react-icons/fa'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from 'next/router'
+
 
 export default function EventPage({ evt }) {
-  const deleteEvent = (e) => {
-    console.log('delete')
+  const router = useRouter();
+  const deleteEvent = async (e) => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: 'DELETE'
+      })
+      const data = await res.json()
+      if (!res.ok) {
+        toast.error(data.message)
+      } else {
+        router.push('/events')
+      }
+    }
   }
-  return (
-    <Layout>
-      <div className={styles.event}>
-        <div className={styles.controls}>
-          <Link href={`/events/edit/${evt.id}`}>
-            <a>
-              <FaPencilAlt />
-              Edit Event
+    return (
+      <Layout>
+        <div className={styles.event}>
+          <div className={styles.controls}>
+            <Link href={`/events/edit/${evt.id}`}>
+              <a>
+                <FaPencilAlt />
+                Edit Event
+              </a>
+            </Link>
+            <a href="#" className={styles.delete} onClick={deleteEvent}>
+              <FaTimes /> Delete Event
             </a>
-          </Link>
-          <a href="#" className={styles.delete} onClick={deleteEvent}>
-            <FaTimes /> Delete Event
-          </a>
-        </div>
-        <span>
-          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
-        </span>
-        <h1>{evt.name}</h1>
-        {evt.image && (
-          <div className={styles.image}>
-            <Image src={evt.image.formats.medium.url} width={960} height={600} />
+          </div>
+          <span>
+            {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
+          </span>
+          <h1>{evt.name}</h1>
+          <ToastContainer />
+          {evt.image && (
+            <div className={styles.image}>
+              <Image src={evt.image.formats.medium.url} width={960} height={600} />
             </div>
-        )}
-        <h3>Performers:</h3>
-        <p>{evt.performers}</p>
-        <h3>Description:</h3>
-        <p>{evt.description}</p>
-        <h3>Venue: {evt.venue}</h3>
-        <p>{evt.address}</p>
-        <Link href="/events">
-          <a className={styles.back}>{'<'} GO BACK</a>
-        </Link>
+          )}
+          <h3>Performers:</h3>
+          <p>{evt.performers}</p>
+          <h3>Description:</h3>
+          <p>{evt.description}</p>
+          <h3>Venue: {evt.venue}</h3>
+          <p>{evt.address}</p>
+          <Link href="/events">
+            <a className={styles.back}>{'<'} GO BACK</a>
+          </Link>
         </div>
-    </Layout>
-  );
-}
+      </Layout>
+    );
+  }
 
-export async function getServerSideProps({ query: { slug } }) {
-  const res = await fetch(`${API_URL}/events?slug=${slug}`);
-  const events = await res.json();
-  return {
-    props: {
-      evt: events[0],
-    },
-  };
-}
+
+  export async function getServerSideProps({ query: { slug } }) {
+    const res = await fetch(`${API_URL}/events?slug=${slug}`);
+    const events = await res.json();
+    return {
+      props: {
+        evt: events[0],
+      },
+    };
+  }
+
